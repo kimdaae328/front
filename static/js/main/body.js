6eef3a43536799107f610be83988770fec2c85b6
ccocco55 <ccocco55@gmail.com>
2025-07-24 17:10:58 +0900

    메인 바디 80% 완성#2

--- static/js/main/body.js
+++ static/js/main/body.js
@@ -1,3 +1,7 @@
+// 배너
+
+// 배너 멈춤 시작 버튼
+
 const button = document.querySelector(".start-stop");
 button.classList.add("start-mode"); // 초기 상태
 
@@ -5,6 +9,8 @@ button.addEventListener("click", () => {
     button.classList.toggle("start-mode");
     button.classList.toggle("stop-mode");
 });
+
+// 배너 무한 슬라이드
 
 const banner = document.querySelector("div.swiper-wrapper");
 const arrows = document.querySelectorAll("button.banner-LR");
@@ -21,16 +27,16 @@ banner.prepend(lastClone);
 let count = 1;
 const slideWidth = 1900;
 
-banner.style.transform = `translateX(-${slideWidth * count}px)`;
+banner.style.transform = `translate(-${slideWidth * count}px)`;
 
 const autoSlide = () => {
     count++;
-    banner.style.transform = `translateX(-${slideWidth * count}px)`;
+    banner.style.transform = `translate(-${slideWidth * count}px)`;
     banner.style.transition = `transform 0.5s`;
 
     if (count === totalSlides + 1) {
         setTimeout(() => {
-            banner.style.transform = `translateX(-${slideWidth * count}px)`;
+            banner.style.transform = `translate(-${slideWidth * count}px)`;
             banner.style.transition = `transform 0s`;
         }, 500);
         count = 1;
@@ -40,6 +46,8 @@ const autoSlide = () => {
 let autoSlideInterval = setInterval(autoSlide, 3000);
 let arrowCheck = true;
 
+// 배너 화살표 클릭시 다음 페이지
+
 arrows.forEach((arrow) => {
     arrow.addEventListener("click", (e) => {
         if (!arrowCheck) return;
@@ -48,11 +56,11 @@ arrows.forEach((arrow) => {
 
         if (arrow.classList.contains("main-banner-button-left")) {
             count--;
-            banner.style.transform = `translateX(-${slideWidth * count}px)`;
+            banner.style.transform = `translate(-${slideWidth * count}px)`;
             banner.style.transition = `transform 0.5s`;
             if (count === 0) {
                 setTimeout(() => {
-                    banner.style.transform = `translateX(-${
+                    banner.style.transform = `translate(-${
                         slideWidth * count
                     }px)`;
                     banner.style.transition = `transform 0s`;
@@ -61,12 +69,12 @@ arrows.forEach((arrow) => {
             }
         } else {
             count++;
-            banner.style.transform = `translateX(-${slideWidth * count}px)`;
+            banner.style.transform = `translate(-${slideWidth * count}px)`;
             banner.style.transition = `transform 0.5s`;
 
             if (count === totalSlides + 1) {
                 setTimeout(() => {
-                    banner.style.transform = `translateX(-${
+                    banner.style.transform = `translate(-${
                         slideWidth * count
                     }px)`;
                     banner.style.transition = `transform 0s`;
@@ -81,4 +89,53 @@ arrows.forEach((arrow) => {
             arrowCheck = true;
         }, 500);
     });
+});
+
+// 지금 가장 많이 담는 특가
+
+const productList = document.querySelectorAll("div.product-card");
+const listCount = Math.ceil(productList.length / 4);
+const backButton = document.querySelector("button.product-list-button-left");
+const nextButton = document.querySelector("button.product-list-button-right");
+const showProduct = document.querySelector(".product-wrap");
+
+let productCount = 0;
+const productSlideWidth = 1050;
+
+// 다음 슬라이드로 이동
+nextButton.addEventListener("click", (e) => {
+    if (productCount < listCount - 1) {
+        productCount++;
+        showProduct.style.transform = `translate(-${
+            productSlideWidth * productCount
+        }px)`;
+        showProduct.style.transition = `transform 0.5s`;
+
+        backButton.style.display = productCount > 0 ? "block" : "none";
+        nextButton.style.display =
+            productCount >= listCount - 1 ? "none" : "block";
+    }
+
+    if (productCount === listCount - 1) {
+        const move =
+            productSlideWidth * productCount - (productSlideWidth - 320);
+        showProduct.style.transform = `translate(-${move}px)`;
+    }
+});
+
+// 이전 슬라이드로 이동
+backButton.addEventListener("click", (e) => {
+    if (productCount > 0) {
+        productCount--;
+        showProduct.style.transform = `translate(-${
+            productSlideWidth * productCount
+        }px)`;
+        showProduct.style.transition = `transform 0.5s`;
+    }
+    if (productCount <= 0) {
+        backButton.style.display = "none";
+    }
+    if (productCount < listCount - 1) {
+        nextButton.style.display = "block";
+    }
 });
