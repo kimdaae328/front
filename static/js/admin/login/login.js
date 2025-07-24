81a1d195737ae4c249a06e3394776e70c6896ef4
duddls9932 <duddls9932@gmail.com>
2025-07-24 11:21:14 +0900

    로그인 창 완성 close #3

--- static/js/admin/login/login.js
+++ static/js/admin/login/login.js
@@ -1,5 +1,11 @@
-document.querySelectorAll(".boot-check-box").forEach((box) => {
-    box.addEventListener("click", () => {
-        box.classList.toggle("checked");
-    });
+const checkbox = document.getElementById("auto-login-checkbox");
+const icon = document.querySelector(".mdi.mdi-check");
+checkbox.addEventListener("change", (e) => {
+    console.log(e.target.checked);
+    console.log(icon);
+    if (e.target.checked) {
+        icon.style.display = "inline-block";
+    } else {
+        icon.style.display = "none";
+    }
 });
