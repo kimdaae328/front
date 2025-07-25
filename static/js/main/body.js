e2e72372ba71970b3581b8608966777dfbad0271
ccocco55 <ccocco55@gmail.com>
2025-07-25 09:01:29 +0900

    main/body update#2

--- static/js/main/body.js
+++ static/js/main/body.js
@@ -15,7 +15,7 @@ button.addEventListener("click", () => {
 const banner = document.querySelector("div.swiper-wrapper");
 const arrows = document.querySelectorAll("button.banner-LR");
 
-const slides = document.querySelectorAll(".swiper-slide"); // 복제 전 슬라이드 개수
+const slides = document.querySelectorAll(".swiper-slide");
 const totalSlides = slides.length;
 
 const firstClone = slides[0].cloneNode(true);
@@ -36,10 +36,10 @@ const autoSlide = () => {
 
     if (count === totalSlides + 1) {
         setTimeout(() => {
-            banner.style.transform = `translate(-${slideWidth * count}px)`;
+            count = 1;
             banner.style.transition = `transform 0s`;
+            banner.style.transform = `translate(-${slideWidth * count}px)`;
         }, 500);
-        count = 1;
     }
 };
 
@@ -50,7 +50,9 @@ let arrowCheck = true;
 
 arrows.forEach((arrow) => {
     arrow.addEventListener("click", (e) => {
-        if (!arrowCheck) return;
+        if (!arrowCheck) {
+            return;
+        }
         arrowCheck = false;
         clearInterval(autoSlideInterval);
 
@@ -91,51 +93,100 @@ arrows.forEach((arrow) => {
     });
 });
 
-// 지금 가장 많이 담는 특가
+// // 지금 가장 많이 담는 특가
 
-const productList = document.querySelectorAll("div.product-card");
-const listCount = Math.ceil(productList.length / 4);
-const backButton = document.querySelector("button.product-list-button-left");
-const nextButton = document.querySelector("button.product-list-button-right");
-const showProduct = document.querySelector(".product-wrap");
+// const productList = document.querySelectorAll("div.product-card");
+// const listCount = Math.ceil(productList.length / 4);
+// const backButton = document.querySelector("button.product-list-button-left");
+// const nextButton = document.querySelector("button.product-list-button-right");
+// const showProduct = document.querySelector(".product-wrap");
 
-let productCount = 0;
-const productSlideWidth = 1050;
+// let productCount = 0;
+// const productSlideWidth = 1068;
 
-// 다음 슬라이드로 이동
-nextButton.addEventListener("click", (e) => {
-    if (productCount < listCount - 1) {
-        productCount++;
-        showProduct.style.transform = `translate(-${
-            productSlideWidth * productCount
-        }px)`;
-        showProduct.style.transition = `transform 0.5s`;
+// // 다음 슬라이드로 이동
+// nextButton.addEventListener("click", (e) => {
+//     if (productCount < listCount - 1) {
+//         productCount++;
+//         showProduct.style.transform = `translate(-${
+//             productSlideWidth * productCount
+//         }px)`;
+//         showProduct.style.transition = `transform 0.5s`;
 
-        backButton.style.display = productCount > 0 ? "block" : "none";
-        nextButton.style.display =
-            productCount >= listCount - 1 ? "none" : "block";
-    }
+//         backButton.style.display = productCount > 0 ? "block" : "none";
+//         nextButton.style.display =
+//             productCount >= listCount - 1 ? "none" : "block";
+//     }
 
-    if (productCount === listCount - 1) {
-        const move =
-            productSlideWidth * productCount - (productSlideWidth - 320);
-        showProduct.style.transform = `translate(-${move}px)`;
-    }
-});
+//     if (productCount === listCount - 1) {
+//         const move =
+//             productSlideWidth * productCount - (productSlideWidth - 320);
+//         showProduct.style.transform = `translate(-${move}px)`;
+//     }
+// });
 
-// 이전 슬라이드로 이동
-backButton.addEventListener("click", (e) => {
-    if (productCount > 0) {
-        productCount--;
-        showProduct.style.transform = `translate(-${
-            productSlideWidth * productCount
-        }px)`;
-        showProduct.style.transition = `transform 0.5s`;
-    }
-    if (productCount <= 0) {
-        backButton.style.display = "none";
-    }
-    if (productCount < listCount - 1) {
-        nextButton.style.display = "block";
-    }
-});
+// // 이전 슬라이드로 이동
+// backButton.addEventListener("click", (e) => {
+//     if (productCount > 0) {
+//         productCount--;
+//         showProduct.style.transform = `translate(-${
+//             productSlideWidth * productCount
+//         }px)`;
+//         showProduct.style.transition = `transform 0.5s`;
+//     }
+//     if (productCount <= 0) {
+//         backButton.style.display = "none";
+//     }
+//     if (productCount < listCount - 1) {
+//         nextButton.style.display = "block";
+//     }
+// });
+
+// // 실시간 인기랭킹
+
+// const rankingList = document.querySelectorAll("div.ranking-swiper-slide");
+// const rankingCount = Math.ceil(rankingList.length / 5);
+// const rankingBackButton = document.querySelector("button.ranking-button-left");
+// const rankingNextButton = document.querySelector("button.ranking-button-right");
+// const rankingShowProduct = document.querySelector(".ranking-swiper-wrapper");
+
+// let rankingProductCount = 0;
+// const rankingSlideWidth = 1075;
+
+// // 다음 슬라이드로 이동
+// rankingNextButton.addEventListener("click", (e) => {
+//     if (rankingProductCount < rankingCount - 1) {
+//         rankingProductCount++;
+
+//         const move = rankingSlideWidth * rankingProductCount;
+//         rankingShowProduct.style.transform = `translateX(-${move}px)`;
+//         rankingShowProduct.style.transition = "transform 0.5s";
+
+//         rankingBackButton.style.display =
+//             rankingProductCount > 0 ? "block" : "none";
+//         rankingNextButton.style.display =
+//             rankingProductCount >= rankingCount ? "none" : "block";
+//     } else if (rankingProductCount === rankingCount - 1) {
+//         const move =
+//             rankingSlideWidth * rankingProductCount -
+//             (rankingProductCount - 200);
+//         rankingShowProduct.style.transform = `translate(-${move}px)`;
+//     }
+// });
+
+// // 이전 슬라이드로 이동
+// rankingBackButton.addEventListener("click", (e) => {
+//     if (rankingProductCount > 0) {
+//         rankingProductCount--;
+//         rankingShowProduct.style.transform = `translate(-${
+//             rankingSlideWidth * rankingProductCount
+//         }px)`;
+//         rankingShowProduct.style.transition = `transform 0.5s`;
+//     }
+//     if (rankingProductCount <= 0) {
+//         rankingBackButton.style.display = "none";
+//     }
+//     if (rankingProductCount < rankingCount - 1) {
+//         rankingNextButton.style.display = "block";
+//     }
+// });
