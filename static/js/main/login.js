f16e66abb01efb3862d7df44998184bdf1862ba4
ccocco55 <ccocco55@gmail.com>
2025-07-26 16:04:41 +0900

    main.login fin close #39

--- static/js/main/login.js
+++ static/js/main/login.js
@@ -6,9 +6,16 @@ openButtons.forEach((btn) => {
         const targetSelector = btn.dataset.target;
         const targetModal = document.querySelector(targetSelector);
         const htmlScroll = document.querySelector("html");
+        const failMessage = document.querySelector("div.fail-login");
+
         if (targetModal) {
             targetModal.style.display = "block";
             htmlScroll.style.overflow = "hidden";
+            if ((targetModal.contain = "popup2")) {
+                failMessage.innerHTML = `<div class="fail-login-inner">
+                                            <p class="fail-login-p">이메일/비밀번호가 일치하지 않습니다.</p>
+                                        </div>`;
+            }
         }
     });
 });
