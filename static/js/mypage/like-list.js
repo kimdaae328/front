3b075e0ec33f2d36bb7f4ab236d6c127b0ae90a1
ccocco55 <ccocco55@gmail.com>
2025-08-03 23:45:53 +0900

    mypage/like-list close #90

--- static/js/mypage/like-list.js
+++ static/js/mypage/like-list.js
@@ -1,5 +1,3 @@
-//
-
 // 사이드 필터 드롭다운
 const filterDropdownButtons = document.querySelectorAll(
     ".filter-category-list .dropdown-btn"
@@ -14,6 +12,15 @@ scrollTopButton.addEventListener("click", () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
 });
 
+// 총 개수
+const likeTotal = document.querySelector(".like-total Strong");
+const list = document.querySelector(".list-inner");
+
+const updateTotalCount = () => {
+    const count = list.querySelectorAll(".available, .unusable").length;
+    likeTotal.innerText = count;
+};
+
 // 최근본 상품
 const recentlyButtons = document.querySelectorAll(
     ".recently-viewed-section button"
@@ -27,6 +34,69 @@ recentlyButtons.forEach((button) => {
             container.scrollBy({ top: -150, behavior: "smooth" });
         } else if (btn.classList.contains("next")) {
             container.scrollBy({ top: 150, behavior: "smooth" });
+        }
+    });
+});
+
+// 팝업 수량 카운트
+const quantityBoxes = document.querySelectorAll(".product-quantity-box");
+
+quantityBoxes.forEach((box) => {
+    const plusBtn = box.querySelector(".quantity-btn.plus");
+    const minusBtn = box.querySelector(".quantity-btn.minus");
+    const countEl = box.querySelector(".count");
+    minusBtn.disabled = true;
+
+    plusBtn.addEventListener("click", () => {
+        let count = Number(countEl.textContent);
+        count++;
+        countEl.textContent = count;
+        minusBtn.disabled = count <= 0;
+    });
+
+    minusBtn.addEventListener("click", () => {
+        let count = Number(countEl.textContent);
+        if (count > 0) count--;
+        countEl.textContent = count;
+        minusBtn.disabled = count == 0;
+    });
+});
+// 팝업
+const openButtons = document.querySelectorAll(".popup-trigger");
+const closeButtons = document.querySelectorAll(".popup-close");
+
+openButtons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        const targetSelector = btn.dataset.target;
+        const targetModal = document.querySelector(targetSelector);
+        const htmlScroll = document.querySelector("html");
+        if (targetModal) {
+            targetModal.style.display = "block";
+            htmlScroll.style.overflow = "hidden";
+        }
+    });
+});
+
+closeButtons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        const targetModal = btn.closest(".popup-wrapper");
+        const htmlScroll = document.querySelector("html");
+        if (targetModal) {
+            targetModal.style.display = "none";
+            htmlScroll.style.overflow = "";
+        }
+    });
+});
+
+// 삭제 버튼
+const removeButtons = document.querySelectorAll(".remove");
+
+removeButtons.forEach((removeButton) => {
+    removeButton.addEventListener("click", function (e) {
+        const productBox = this.closest(".available, .unusable");
+        if (productBox) {
+            productBox.remove();
+            updateTotalCount();
         }
     });
 });
