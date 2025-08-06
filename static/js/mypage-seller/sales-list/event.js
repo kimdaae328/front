8c0cbc95d67336829dca0933d3314d8d3539645b
alzkdpf000 <alzkdpf000@gmail.com>
2025-08-06 15:49:34 +0900

    mypage-seller/sales-list event #128

---
+++ static/js/mypage-seller/sales-list/event.js
@@ -0,0 +1,31 @@
+// 개월 드롭다운
+const dropdown = document.querySelector(".custom-dropdown");
+const toggle = dropdown.querySelector(".dropdown-toggle");
+const menu = dropdown.querySelector(".dropdown-menu");
+const items = menu.querySelectorAll("li");
+
+toggle.addEventListener("click", (e) => {
+    toggle.classList.toggle("active");
+});
+
+items.forEach((item) => {
+    item.addEventListener("click", (e) => {
+        toggle.firstElementChild.innerHTML = `<span>${item.textContent}</span>`;
+        toggle.classList.remove("active");
+    });
+});
+
+// 외부 클릭 시 개월 드롭다운 닫기
+document.addEventListener("click", (e) => {
+    if (!dropdown.contains(e.target)) {
+        toggle.classList.remove("active");
+    }
+});
+
+// 검색 취소 버튼
+const cancelButton = document.querySelector(".seller-search-cancel");
+const searchInput = document.querySelector(".search-box-input");
+
+cancelButton.addEventListener("click", (e) => {
+    searchInput.value = "";
+});
